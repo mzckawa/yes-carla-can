4596c1c69ad27312e0aaebea553283e13a265402
mzckawa <5689.melzion.cmr@gmail.com>
2026-08-13 14:22:35 -0300

    Update collision.py

--- sensors/collision.py
+++ sensors/collision.py
@@ -1,43 +1,27 @@
-import collections
-import math
 import weakref
-
 import carla
 
-from gui.functions import get_actor_display_name
 
-
-class CollisionSensor(object):
-    def __init__(self, parent_actor, hud, can_net):
+class GnssSensor(object):
+    def __init__(self, parent_actor, can_net):
         self.sensor = None
-        self.history = []
         self._parent = parent_actor
-        self.hud = hud
         self._can_net = can_net
+        self.lat = 0.0
+        self.lon = 0.0
         world = self._parent.get_world()
-        bp = world.get_blueprint_library().find('sensor.other.collision')
-        self.sensor = world.spawn_actor(bp, carla.Transform(), attach_to=self._parent)
+        bp = world.get_blueprint_library().find('sensor.other.gnss')
+        self.sensor = world.spawn_actor(bp, carla.Transform(carla.Location(x=1.0, z=2.8)), attach_to=self._parent)
         # We need to pass the lambda a weak reference to self to avoid circular
         # reference.
         weak_self = weakref.ref(self)
-        self.sensor.listen(lambda event: CollisionSensor._on_collision(weak_self, event))
-
-    def get_collision_history(self):
-        history = collections.defaultdict(int)
-        for frame, intensity in self.history:
-            history[frame] += intensity
-        return history
+        self.sensor.listen(lambda event: GnssSensor._on_gnss_event(weak_self, event))
 
     @staticmethod
-    def _on_collision(weak_self, event):
+    def _on_gnss_event(weak_self, event):
         self = weak_self()
         if not self:
             return
-        actor_type = get_actor_display_name(event.other_actor)
-        self.hud.notification('Collision with %r' % actor_type)
-        impulse = event.normal_impulse
-        intensity = math.sqrt(impulse.x**2 + impulse.y**2 + impulse.z**2)
-        self.history.append((event.frame, intensity))
-        if len(self.history) > 4000:
-            self.history.pop(0)
-        self._can_net.send_collision_msg(intensity)
+        self.lat = event.latitude
+        self.lon = event.longitude
+        self._can_net.send_gnss_msg(self.lat, self.lon)
